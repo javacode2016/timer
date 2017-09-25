3438ffd3f5b8b18dccf60b855244b85d840ea807
javacode2016 <javacode@mail.ru>
2017-09-25 21:06:26 +0200

    start works,,, almost

---
+++ app/src/main/java/yahoo/helloworld/Timer.java
@@ -0,0 +1,16 @@
+package yahoo.helloworld;
+
+/**
+ * Created by Admin on 25.09.2017.
+ */
+
+public class Timer {
+    private int delay;
+    Timer(int sec) {delay = 1000 * sec; }
+
+}
+class Regulator  {
+    Timer t = new Timer(1);
+
+    //забей, это таймер от светофора)
+}
--- app/src/main/java/yahoo/helloworld/first.java
+++ app/src/main/java/yahoo/helloworld/first.java
@@ -2,15 +2,30 @@ package yahoo.helloworld;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Admin on 25.09.2017.
  */
 
 public class first extends Activity {
+    Button btnStart, btnStop, btnPause, btnReset;
+    TextView tvTime;
+    Timer timer;
+    // Get a handler that can be used to post to the main thread
+    Handler mainHandler;
+
+    private int minutes, seconds;
 
 
     @Override
@@ -18,6 +33,52 @@ public class first extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("HWA", getResources().getString(R.string.yabadabadoo));
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        timer = new Timer();
+
+        mainHandler = new Handler(getMainLooper());
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.schedule(new timertask(), 0, 1000);
+            }
+        });
+
+
+//вац
+
+    }
+
+    private void tick() {
+
+
+        Log.d("TIMER", "elapsed: " + minutes + "minutes " + seconds + " seconds");
+//я понял, на javascript создавал таймер, такой же принцип обнуления секунд
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                tvTime.setText(minutes + ":" + seconds);
+            } // This is your code
+        };
+        mainHandler.post(myRunnable);
+        if (seconds < 59) {
+            seconds++;
+        } else {
+            minutes++;
+            seconds = 0;
+        }
+    }
+
+    class timertask extends TimerTask {
+        @Override
+        public void run() {
+            tick();
+        }
     }
 }
